c9950fb310bb8639cbf4ed06a2cb6febf4e77a56
kektram <77460470+kektram@users.noreply.github.com>
2022-07-06 13:53:15 -0300

    Add files via upload

--- Kek's menu.lua
+++ Kek's menu.lua
@@ -5,7 +5,7 @@ if __kek_menu_version then
 	return
 end
 
-__kek_menu_version = "0.4.8.0 beta 17"
+__kek_menu_version = "0.4.8.0 beta 17 test"
 __kek_menu_debug_mode = false
 __kek_menu_participate_in_betas = false
 __kek_menu_check_for_updates = false
