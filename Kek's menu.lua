574cfb929ac7cbd0037e41b84ac847dbac55f1f3
kektram <77460470+kektram@users.noreply.github.com>
2022-07-07 22:02:39 +0200

    Update Kek's menu.lua

--- Kek's menu.lua
+++ Kek's menu.lua
@@ -4,7 +4,7 @@ if __kek_menu_version then
 	return
 end
 
-__kek_menu_version = "0.4.8.0 beta 19 test"
+__kek_menu_version = "0.4.8.0 beta 19"
 __kek_menu_debug_mode = false
 __kek_menu_participate_in_betas = false
 __kek_menu_check_for_updates = false
