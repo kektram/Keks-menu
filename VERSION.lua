21cc4c40e89dae2cdd24c16e1d5119b19ba1781c
kektram <77460470+kektram@users.noreply.github.com>
2022-07-15 04:28:01 +0200

    Update and rename VERSION.txt to VERSION.lua

---
+++ VERSION.lua
@@ -0,0 +1,38 @@
+return {
+  version = "0.4.8.0.b28",
+  force_update_keks_menu_autoexec = false,
+  libs = {
+    "Kek's Language",
+    "Kek's Enums",
+    "Kek's Settings",
+    "Kek's Memoize",
+    "Kek's Essentials",
+    "Kek's Vehicle mapper",
+    "Kek's Ped mapper",
+    "Kek's Object mapper",
+    "Kek's Globals",
+    "Kek's Weapon mapper",
+    "Kek's Location mapper",
+    "Kek's Keys and input",
+    "Kek's Drive style mapper",
+    "Kek's Menyoo spawner",
+    "Kek's Entity functions",
+    "Kek's Trolling entities",
+    "Kek's Custom upgrades",
+    "Kek's Admin mapper",
+    "Kek's Menyoo saver",
+    "Kek's Natives",
+    "Debugger.lua"
+  },
+  language_files = {
+    "Arabic.txt",
+    "Chinese.txt",
+    "Dutch.txt",
+    "French.txt",
+    "German.txt",
+    "Korean.txt",
+    "Norwegian.txt",
+    "Spanish.txt",
+    "Swedish.txt"
+  }
+}
